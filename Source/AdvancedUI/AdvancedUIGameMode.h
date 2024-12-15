550ebe095c3665d2a8bc2246c5862f5eb48229a5
Andrew Pol <87779923+BONAM0RS@users.noreply.github.com>
2024-12-15 11:39:37 +0200

    Init project, enabled CommonUI and ViewModel plugins

---
+++ Source/AdvancedUI/AdvancedUIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvancedUIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAdvancedUIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAdvancedUIGameMode();
+};
+
+
+
